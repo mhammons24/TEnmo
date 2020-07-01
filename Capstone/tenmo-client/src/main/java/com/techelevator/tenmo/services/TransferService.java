9dacfa7b199baa61f1cf1a2f8be1f95b7b55b3e7
Correna Sprowls <correnarsprowls@gmail.com>
2020-07-01 13:36:55 -0400

    tests for jdbc transfer completed

--- Capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ Capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -1,8 +1,13 @@
 package com.techelevator.tenmo.services;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+
+import com.techelevator.hotels.models.Reservation;
 import com.techelevator.tenmo.models.Transfer;
 import com.techelevator.view.ConsoleService;
 
@@ -10,21 +15,28 @@ public class TransferService {
 	private ConsoleService console;
 	private RestTemplate restTemplate = new RestTemplate();
 	private String baseUrl;
-	
+
 	public TransferService(ConsoleService console, String baseUrl) {
 		this.console = console;
 		this.baseUrl = baseUrl;
 	}
-	
+
 	public void approveTransfer(int transferId) {
-		
+
 		try {
 			restTemplate.put(baseUrl + "transfers/approve/" + transferId, Transfer.class);
-		    } catch (RestClientResponseException ex) {
-		      console.printError(ex.getRawStatusCode() + " : " + ex.getStatusText());
-		    } catch (ResourceAccessException ex) {
-		      console.printError(ex.getMessage());
-		    }
+		} catch (RestClientResponseException ex) {
+			console.printError(ex.getRawStatusCode() + " : " + ex.getStatusText());
+		} catch (ResourceAccessException ex) {
+			console.printError(ex.getMessage());
+		}
 
+	}
+	
+	private HttpEntity<Transfer> makeHeader(Transfer transfer) {
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+	    HttpEntity<Transfer> entity = new HttpEntity<>(transfer, headers);
+	    return entity;
 	}
 }
